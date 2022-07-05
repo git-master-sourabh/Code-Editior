c55a2d7e749f49356f1d3b5bad1d0b33306c3650
Your Name <you@example.com>
2022-07-05 18:58:23 +0530

    ready to deploy 1

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const {Server} = require('socket.io');
 const ACTIONS = require('./src/Actions');
 
@@ -8,6 +9,10 @@ const server = http.createServer(app);
 
 const io = new Server(server);
 
+app.use(express.static('build'));
+app.use((req,res,next)=>{
+     res.sendFile(path.join(__dirname, 'build','index.html'));
+})
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
     // Map
